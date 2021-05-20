646124209ec637ef45ac3650053d508154215895
Marcos Paulo <marcos.p.mcruz@outlook.com>
2021-05-20 10:56:07 -0300

    Desenvolvendo operacao de criacao de pessoas

---
+++ src/main/java/com/example/personapi/service/PersonService.java
@@ -0,0 +1,27 @@
+package com.example.personapi.service;
+
+import com.example.personapi.dto.MessageResponseDTO;
+import com.example.personapi.model.Person;
+import com.example.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with id: " + savedPerson.getId())
+                .build();
+    }
+}
